091087a95785fd0b535033f9c5cfc34008dff73f
Orce <apostolovski.orce@hotmail.com>
2019-11-25 22:03:22 +0100

    petti

--- handlers/auth.js
+++ handlers/auth.js
@@ -1,6 +1,9 @@
 const mUsers = require('../models/users');
 const vUsers = require('../validators/users');
 var validator = require('node-input-validator');
+var bcrypt = require('bcryptjs');
+var jwt = require ('jsonwebtoken');
+const config = require ('../config/index.js')
 
 
 const register = (req, res) => {
@@ -8,7 +11,20 @@ const register = (req, res) => {
     v.check ()
     .then(matched => {
         if(matched) {
-            return mUsers.createUser(req.body);
+            bcrypt.genSalt(10, function(err, salt) {
+                if(err) {
+                    throw new Error(err);
+                    return;
+                }
+                bcrypt.hash(req.body.password, salt, function(err, hash) {
+                    if (err){
+                        throw new Error(err);
+                        return;
+                    }
+                    return mUsers.createUser({...req.body, password: hash});
+                });
+            });
+           
     } else {
         throw new Error('Validation failed')
         }
@@ -25,12 +41,36 @@ const register = (req, res) => {
 
 
 
-const login= (req, res) => {
-    return res.status(200).send('ok');
+const login = (req, res) => {
+    mUsers.getUserPasswordByEmail(req.body.email)
+    .then((data) => {
+        // console.log(data);
+        bcrypt.compare(req.body.password, data.password, function (err,rez) {
+            if(err){
+                return res.status(500).send('Could not compare password');
+            } 
+            if(rez) {
+                // return res.status(200).send('ok');
+                var tokenData = {
+                    id: rez._id,
+                    full_name: `${rez.first_name} ${rez.last_name}`,
+                    email: rez.email
+                };
+                var token = jwt.sign(tokenData, config.getConfig('jwt').key);
+                return res.status(200).send({jwt: token});
+            }
+            return res.status(404).send('not found');
+        });
+    })
+
+    .catch(err => {
+        console.log(err);
+        return res.status(500).send('Could not get user');
+    });
 }
 
 
-const renew= (req, res) => {
+const renew = (req, res) => {
     return res.status(200).send('ok');
 }
 
