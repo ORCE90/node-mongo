5a01df8fad38be842cd3b09b6b8d086626dff7ae
Orce <apostolovski.orce@hotmail.com>
2019-12-25 21:49:39 +0100

    change 2

--- services/proxy.js
+++ services/proxy.js
@@ -16,4 +16,16 @@ api.all('/api/v1/filmovi/*', (req, res) => {
     apiProxy.web(req, res, {target: 'http://localhost: 8000'});
 });
 
-api.listen(5000);
+api.all('/*', (req, res) => {
+    res.status(404).send('Not Found!');
+});
+
+api.listen(process.env.PORT, err => {
+    if(err) {
+        console.log('could not start server');
+        console.log(err);
+        return;
+    }
+
+    console.log('server started successfully on port 5000');
+});
